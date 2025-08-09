5537569706a72f9fda7d94f47add0d653c4895f6
Cierra-Runis <byrdsaron@gmail.com>
2025-08-09 22:42:08 +0800

    feat: Add CORS middleware configuration to support API requests

--- server/router/router.go
+++ server/router/router.go
@@ -4,11 +4,19 @@ import (
 	"server/handler"
 
 	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3/middleware/cors"
 	"github.com/kiuber/gofiber3-contrib/websocket"
 )
 
 func Setup(userHandler *handler.UserHandler) *fiber.App {
 	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowCredentials: true,
+	}))
 
 	api := app.Group("/api")
 	api.Get("/health", handler.GetHealth)
