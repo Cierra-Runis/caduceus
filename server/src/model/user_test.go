abde284fe4db3e6d90c52f4eb2f70994eb1766cd
Cierra-Runis <byrdsaron@gmail.com>
2025-08-16 13:48:23 +0800

    test: Rewrite database and model test files with detailed unit tests restructuring

--- server/src/model/user_test.go
+++ server/src/model/user_test.go
@@ -5,52 +5,12 @@ import (
 	"testing"
 	"time"
 
-	"github.com/golang-jwt/jwt/v5"
 	"github.com/stretchr/testify/assert"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/integration/mtest"
 )
-
-func TestGenerateToken(t *testing.T) {
-	user := &User{
-		ID:       primitive.NewObjectID(),
-		Username: "test_user",
-	}
-	secret := "test_secret"
-
-	t.Run("successful_token_generation", func(t *testing.T) {
-		token, err := GenerateToken(user, secret)
-		assert.NoError(t, err)
-		assert.NotEmpty(t, token)
-
-		parsedToken, err := jwt.ParseWithClaims(token, &JwtCustomClaims{}, func(token *jwt.Token) (interface{}, error) {
-			return []byte(secret), nil
-		})
-		assert.NoError(t, err)
-		assert.True(t, parsedToken.Valid)
-
-		claims := parsedToken.Claims.(*JwtCustomClaims)
-		assert.Equal(t, user.ID, claims.ID)
-		assert.Equal(t, user.Username, claims.Username)
-		assert.Equal(t, "Token", claims.Subject)
-	})
-
-	t.Run("token_expiration", func(t *testing.T) {
-		token, err := GenerateToken(user, secret)
-		assert.NoError(t, err)
-
-		parsedToken, err := jwt.ParseWithClaims(token, &JwtCustomClaims{}, func(token *jwt.Token) (interface{}, error) {
-			return []byte(secret), nil
-		})
-		assert.NoError(t, err)
-
-		claims := parsedToken.Claims.(*JwtCustomClaims)
-		expectedExpiry := time.Now().Add(24 * time.Hour)
-		assert.WithinDuration(t, expectedExpiry, claims.ExpiresAt.Time, time.Minute)
-	})
-}
 
 func TestNewMockUserRepo(t *testing.T) {
 	repo := NewMockUserRepo()
