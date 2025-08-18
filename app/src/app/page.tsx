bc362b26db5fb2e256d536f4fc69ca96355ceeda
Cierra-Runis <byrdsaron@gmail.com>
2025-08-18 21:34:49 +0800

    feat: Redesign Home component layout and add navigation buttons

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,12 +1,28 @@
-import { ServerBadge } from "@/components/badges/ServerBadge";
-import { Avatar } from "@heroui/avatar";
+import { Button } from "@heroui/button";
+import NextLink from 'next/link';
+
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <ServerBadge>
-        <Avatar src="https://avatars.githubusercontent.com/u/29329988" />
-      </ServerBadge>
-    </div>
+    <section className="relative flex flex-col items-center justify-center h-[calc(100vh_-_64px)] overflow-hidden">
+      <section className="z-20 flex flex-col items-center justify-center gap-[18px] sm:gap-6">
+        <div className="text-center text-4xl leading-[1.2] font-bold tracking-tighter sm:text-6xl">
+          <div className="bg-linear-91 from-[hsl(var(--heroui-primary))] to-[hsl(var(--heroui-secondary))] bg-clip-text text-transparent">
+            Caduceus
+          </div>
+        </div>
+        <p className="text-default-500 text-center leading-7 font-normal sm:w-[466px] sm:text-[18px]">
+          An open-source alternative to <NextLink href='https://typst.app'>Typst App</NextLink>.
+        </p>
+        <div className="flex items-center justify-center gap-6">
+          <Button href="/register" as={NextLink} color="primary" variant="ghost" size="lg">
+            Register now!
+          </Button>
+          <Button href="https://github.com/Cierra-Runis/caduceus" as={NextLink} variant="bordered" size="lg">
+            View on GitHub
+          </Button>
+        </div>
+      </section>
+    </section>
   );
 }
