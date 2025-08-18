5224b32b2807a10d482b3dc525d8f9a249b7bb2a
Cierra-Runis <byrdsaron@gmail.com>
2025-08-18 20:24:53 +0800

    feat: Update footer layout and link to LICENSE file

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -6,6 +6,7 @@ import '@/styles/globals.css';
 import { Image } from '@heroui/image';
 import type { Metadata } from 'next';
 import { Saira } from 'next/font/google';
+import NextLink from 'next/link';
 
 const sans = Saira({
   variable: '--font-sans',
@@ -29,27 +30,27 @@ export default function RootLayout({
           <div className='relative h-screen'>
             <NavBar />
             {children}
-            <footer className='flex w-full flex-col mx-auto max-w-7xl px-6 pb-6 md:flex-row-reverse md:justify-between'>
+            <footer className='flex w-full flex-col mx-auto max-w-7xl px-6 pb-4 md:flex-row-reverse md:justify-between'>
               <div className='flex items-center justify-center gap-2'>
                 <ThemeButtons />
               </div>
               <div>
-                <div className='flex items-center justify-center gap-3 md:justify-start'>
+                <div className='flex items-center justify-center gap-4 md:justify-start'>
                   <div className='flex items-center gap-1'>
                     <Image src="favicon.svg" alt='Caduceus Icon' className='w-6 dark:invert'></Image>
                     <span className='text-small font-medium'>Caduceus</span>
                   </div>
-                  <div className='relative max-w-fit min-w-min inline-flex items-center justify-between box-border whitespace-nowrap border-medium border-default bg-transparent h-7 text-small rounded-full text-default-500 border-none px-0'>
+                  <div className='max-w-fit min-w-min inline-flex items-center justify-between box-border whitespace-nowrap border-medium border-default bg-transparent h-7 text-small rounded-full text-default-500 border-none gap-2'>
                     <ServerBadge />
                     {/* TODO: Use `useServerStatus` */}
-                    <span className='flex-1 text-inherit font-normal px-2'>
-                      All systems operational
+                    <span className='flex-1 text-inherit font-normal'>
+                      Backend Status
                     </span>
                   </div>
                 </div>
-                <p className='text-tiny text-default-400 text-center md:text-start'>
-                  © 2024 Acme Inc. All rights reserved.
-                </p>
+                <NextLink href="https://github.com/Cierra-Runis/caduceus/blob/main/LICENSE" className='text-tiny text-default-400 text-center md:text-start'>
+                  MIT Licensed | © 2025 Cierra Runis
+                </NextLink>
               </div>
             </footer>
           </div>
