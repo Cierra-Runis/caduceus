65624ac662f8b7edfa24b80cc72cfb8655ea6662
Cierra-Runis <byrdsaron@gmail.com>
2025-08-28 20:05:37 +0800

    feat: Add internationalization support for Home component and update translations

--- app/src/app/(home)/page.tsx
+++ app/src/app/(home)/page.tsx
@@ -1,7 +1,9 @@
 import { Button } from '@heroui/button';
+import { useTranslations } from 'next-intl';
 import NextLink from 'next/link';
 
 export default function Home() {
+  const t = useTranslations('Home');
   return (
     <section className='relative flex h-[calc(100vh_-_64px)] flex-col items-center justify-center overflow-hidden'>
       <section className='z-20 flex flex-col items-center justify-center gap-[18px] sm:gap-6'>
@@ -11,8 +13,11 @@ export default function Home() {
           </div>
         </div>
         <p className='text-default-500 text-center leading-7 font-normal sm:w-[466px] sm:text-[18px]'>
-          An open-source alternative to{' '}
-          <NextLink href='https://typst.app'>Typst App</NextLink>.
+          {t.rich('description', {
+            typstApp: (chunks) => (
+              <NextLink href='https://typst.app'>{chunks}</NextLink>
+            ),
+          })}
         </p>
         <div className='flex items-center justify-center gap-6'>
           <Button
@@ -22,7 +27,7 @@ export default function Home() {
             size='lg'
             variant='ghost'
           >
-            Register now!
+            {t('registerNow')}
           </Button>
           <Button
             as={NextLink}
@@ -30,7 +35,7 @@ export default function Home() {
             size='lg'
             variant='bordered'
           >
-            View on GitHub
+            {t('viewOnGitHub')}
           </Button>
         </div>
       </section>
