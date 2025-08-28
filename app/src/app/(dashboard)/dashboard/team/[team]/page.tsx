a778b2b1d010a30e22f44eb07abfa52aca2fcc8c
Cierra-Runis <byrdsaron@gmail.com>
2025-08-28 20:05:38 +0800

    refactor: Update Sidebar component to use Tabs for navigation and improve structure

---
+++ app/src/app/(dashboard)/dashboard/team/[team]/page.tsx
@@ -0,0 +1,3 @@
+export default function TeamPage() {
+  return <div className='container mx-auto space-y-6'></div>;
+}
--- app/src/components/Sidebar.tsx
+++ app/src/components/Sidebar.tsx
@@ -1,100 +1,57 @@
 'use client';
 
-import { Listbox, ListboxItem, ListboxSection } from '@heroui/listbox';
-import { ScrollShadow } from '@heroui/scroll-shadow';
-import {
-  IconHome,
-  IconLogout,
-  IconPackage,
-  IconSettings,
-  IconSubtask,
-  IconUser,
-} from '@tabler/icons-react';
-import { useTranslations } from 'next-intl';
-import { usePathname } from 'next/navigation';
+import { Avatar } from '@heroui/avatar';
+import { Button } from '@heroui/button';
+import { Tab, Tabs } from '@heroui/tabs';
+import { IconLogout } from '@tabler/icons-react';
+import NextLink from 'next/link';
+import { useParams } from 'next/navigation';
 
 export default function Sidebar() {
-  const pathname = usePathname();
-  const t = useTranslations('Sidebar');
+  const { team } = useParams();
 
   return (
-    <aside className='transition-width relative flex w-72 flex-col justify-between p-6'>
-      <header className='flex flex-col items-center justify-center gap-4'>
-        <h2 className='text-2xl font-bold'>Caduceus</h2>
-        {/* <User avatarProps={{
-          src: "https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
-        }}
-          description=("Proje{tct Manager")
-          name}="Jane Doe"
-        /> */}
-      </header>
-
-      <ScrollShadow className='flex flex-1 items-center'>
-        <Listbox
-          classNames={{ base: 'w-full' }}
-          selectedKeys={[pathname]}
-          selectionMode='single'
+    <aside className='relative flex flex-col items-center justify-between py-3 transition-all'>
+      <div className='flex flex-col items-center'>
+        <Tabs
+          classNames={{
+            tab: 'w-auto h-auto p-2',
+            tabList: 'space-y-4 bg-transparent',
+          }}
+          isVertical
+          selectedKey={team ? `/dashboard/team/${team}` : '/dashboard'}
         >
-          <ListboxSection title='Overview'>
-            <ListboxItem
-              classNames={{ description: 'text-md' }}
-              description={t('home')}
-              href='/dashboard'
-              key='/dashboard'
-              startContent={<IconHome />}
-            />
-            <ListboxItem
-              classNames={{ description: 'text-md' }}
-              description={t('projects')}
-              href='/dashboard/projects'
-              key='/dashboard/projects'
-              startContent={<IconPackage />}
-            />
-            <ListboxItem
-              classNames={{ description: 'text-md' }}
-              description={t('tasks')}
-              href='/dashboard/tasks'
-              key='/dashboard/tasks'
-              startContent={<IconSubtask />}
-            />
-            <ListboxItem
-              classNames={{ description: 'text-md' }}
-              description={t('teams')}
-              href='/dashboard/teams'
-              key='/dashboard/teams'
-              startContent={<IconUser />}
-            />
-          </ListboxSection>
-          <ListboxSection title='Organization'>
-            <ListboxItem
-              classNames={{ description: 'text-md' }}
-              description={t('members')}
-              href='/dashboard/members'
-              key='/dashboard/members'
-              startContent={<IconUser />}
-            />
-            <ListboxItem
-              classNames={{ description: 'text-md' }}
-              description={t('settings')}
-              href='/dashboard/settings'
-              key='/dashboard/settings'
-              startContent={<IconSettings />}
-            />
-          </ListboxSection>
-        </Listbox>
-      </ScrollShadow>
-
-      <Listbox className='mt-auto'>
-        <ListboxSection>
-          <ListboxItem
-            classNames={{ description: 'text-md' }}
-            description={t('logout')}
-            href='/logout'
-            key='/logout'
-            startContent={<IconLogout />}
+          <Tab
+            as={NextLink}
+            href='/dashboard'
+            key='/dashboard'
+            title={<Avatar src='https://i.pravatar.cc?img=1' />}
           />
-        </ListboxSection>
-      </Listbox>
+
+          <Tab
+            as={NextLink}
+            href='/dashboard/team/grjfkesghzishfkjhwf'
+            key='/dashboard/team/grjfkesghzishfkjhwf'
+            title={<Avatar src='https://i.pravatar.cc?img=2' />}
+          />
+          <Tab
+            as={NextLink}
+            href='/dashboard/team/esglihURGbwkfhaqwih'
+            key='/dashboard/team/esglihURGbwkfhaqwih'
+            title={<Avatar src='https://i.pravatar.cc?img=3' />}
+          />
+        </Tabs>
+      </div>
+
+      <div className='flex flex-col items-center space-y-2'>
+        <Button
+          as={NextLink}
+          href='/logout'
+          isIconOnly
+          startContent={<IconLogout />}
+          variant='light'
+        />
+      </div>
     </aside>
   );
 }
