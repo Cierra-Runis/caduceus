46949929d6552429795ad86fb724315990814c51
Cierra-Runis <byrdsaron@gmail.com>
2025-08-29 22:59:08 +0800

    fix: Improve error message for invalid JSON in request validation

--- server/src/middleware/mod.rs
+++ server/src/middleware/mod.rs
@@ -61,7 +61,7 @@ where
     async fn from_request(req: Request, state: &S) -> Result<Self, Self::Rejection> {
         let Json(value) = Json::<T>::from_request(req, state)
             .await
-            .map_err(|_| AppError::Validation("Invalid JSON".to_string()))?;
+            .map_err(|e| AppError::Validation(format!("Invalid Json: {}", e)))?;
 
         value
             .validate()
