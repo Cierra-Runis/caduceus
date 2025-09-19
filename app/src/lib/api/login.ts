ca2efda15e64f0a3e704530febe9e67fec49cab1
Cierra-Runis <byrdsaron@gmail.com>
2025-09-20 00:03:48 +0800

    feat: Update LoginSchema to include required message in string validation

--- app/src/lib/api/login.ts
+++ app/src/lib/api/login.ts
@@ -4,8 +4,8 @@ import { ApiResponse } from '../response';
 import { AuthPayload } from './register';
 
 export const LoginSchema = z.object({
-  password: z.string().nonempty('Password is required'),
-  username: z.string().nonempty('Username is required'),
+  password: z.string('Password is required').nonempty('Password is required'),
+  username: z.string('Username is required').nonempty('Username is required'),
 });
 
 export type LoginRequest = z.infer<typeof LoginSchema>;
