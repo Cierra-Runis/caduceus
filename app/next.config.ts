0f706ab184b0fc87b1306f9cf5a71ad49d7dce96
Cierra-Runis <byrdsaron@gmail.com>
2025-09-30 00:02:54 +0800

    refactor: Remove rewrites in next.config.ts

--- app/next.config.ts
+++ app/next.config.ts
@@ -8,15 +8,6 @@ const nextConfig: NextConfig = {
       process.env.NODE_ENV === 'production' ? { exclude: ['error'] } : false,
   },
   reactStrictMode: true,
-  async rewrites() {
-    // Proxy the front-end /api/*request to the local Go service (development environment)
-    return [
-      {
-        destination: 'http://localhost:8080/api/:path*',
-        source: '/api/:path*',
-      },
-    ];
-  },
   typedRoutes: true,
 };
 
