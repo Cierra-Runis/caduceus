9cf88fdeca278dfc6fcc4b37c13cc80c32d8cf93
Cierra-Runis <byrdsaron@gmail.com>
2025-09-29 20:17:26 +0800

    feat: Update ESLint configuration to include better-tailwindcss plugin

--- app/eslint.config.mjs
+++ app/eslint.config.mjs
@@ -4,23 +4,47 @@ import eslintPluginJavaScript from '@eslint/js';
 /// https://www.npmjs.com/package/@next/eslint-plugin-next
 /// Official ESLint plugin for Next.js
 import eslintPluginNext from '@next/eslint-plugin-next';
+/// https://github.com/schoero/eslint-plugin-better-tailwindcss
+/// ESLint plugin to help you write better tailwindcss by improving readability with formatting rules and enforcing best practices with linting rules.
+import eslintPluginBetterTailwindCss from 'eslint-plugin-better-tailwindcss';
 /// https://github.com/azat-io/eslint-plugin-perfectionist
 /// ESLint plugin for sorting various data such as objects, imports, types, enums, JSX props, etc.
 import eslintPluginPerfectionist from 'eslint-plugin-perfectionist';
-/// https://github.com/francoismassart/eslint-plugin-tailwindcss
-/// While you can use the official plugin for ordering, this plugin offers more than 5 other rules
-import eslintPluginTailwindCSS from 'eslint-plugin-tailwindcss';
 /// https://typescript-eslint.io/getting-started
 /// Powerful static analysis for JavaScript and TypeScript.
+import { defineConfig } from 'eslint/config';
 import eslintPluginTypeScript from 'typescript-eslint';
 
 /// FIXME: https://github.com/vercel/next.js/issues/73655#issuecomment-3344699670
 const { flatConfig: eslintPluginNextFlatConfig } = eslintPluginNext;
 
-import { defineConfig } from 'eslint/config';
+const eslintPluginBetterTailwindCssConfig = {
+  name: 'better-tailwindcss/',
+  files: ['**/*.{jsx,tsx}'],
+  languageOptions: {
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+      },
+    },
+  },
+  plugins: {
+    'better-tailwindcss': eslintPluginBetterTailwindCss,
+  },
+  rules: {
+    ...eslintPluginBetterTailwindCss.configs['recommended-warn'].rules,
+    ...eslintPluginBetterTailwindCss.configs['recommended-error'].rules,
+  },
+  settings: {
+    'better-tailwindcss': {
+      entryPoint: 'src/styles/globals.css',
+    },
+  },
+};
 
 export default defineConfig([
   {
+    name: 'base/',
     ignores: [
       'node_modules/**',
       '.next/**',
@@ -32,7 +56,7 @@ export default defineConfig([
   eslintPluginJavaScript.configs.recommended,
   eslintPluginTypeScript.configs.strict,
   eslintPluginTypeScript.configs.stylistic,
-  eslintPluginTailwindCSS.configs['flat/recommended'],
+  eslintPluginBetterTailwindCssConfig,
   eslintPluginPerfectionist.configs['recommended-alphabetical'],
   eslintPluginNextFlatConfig.recommended,
 ]);
