19b45fc5fc16a8007f30bd97ebf5fb987984ef4f
Cierra-Runis <byrdsaron@gmail.com>
2025-10-01 00:38:14 +0800

    refactor: Replace 'ky' with 'api' for project data fetching and streamline cookie handling

--- app/src/app/(project)/project/[id]/page.tsx
+++ app/src/app/(project)/project/[id]/page.tsx
@@ -1,8 +1,9 @@
-import ky, { HTTPError } from 'ky';
+import { HTTPError, Options } from 'ky';
 import { cookies } from 'next/headers';
 import { notFound, redirect } from 'next/navigation';
 
 import { ProjectPayload } from '@/lib/api/project';
+import { api } from '@/lib/request';
 import { ApiResponse } from '@/lib/response';
 
 import { ClientPage } from './_components/ClientPage';
@@ -10,16 +11,16 @@ import { ClientPage } from './_components/ClientPage';
 export default async function Page(props: PageProps<'/project/[id]'>) {
   const { id } = await props.params;
   const cookieStore = await cookies();
+  const options: Options = {
+    headers: {
+      cookie: cookieStore.toString(),
+    },
+  };
 
   try {
-    const res = await ky
-      .get(`project/${id}`, {
-        headers: {
-          cookie: cookieStore.toString(),
-        },
-      })
+    const res = await api
+      .get(`project/${id}`, options)
       .json<ApiResponse<ProjectPayload>>();
-
     return <ClientPage project={res.payload} />;
   } catch (error) {
     if (error instanceof HTTPError) {
